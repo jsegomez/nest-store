6acb5ee10303c2905c6e567a32c311af8e1373f3
Josué Eliezer Gómez Soto <jsegomez06@gmail.com>
2021-08-21 00:04:48 -0600

    Trabajando variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 
 // Variables de entorno
-import { environments } from '../environments';
+import { environments } from './environments';
+import config from './config';
 
 // Modulos
 import { UsersModule } from './users/users.module';
@@ -16,13 +17,14 @@ import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+    }),
     UsersModule,
     ProductsModule,
     DatabaseModule,
-    ConfigModule.forRoot({
-      envFilePath: environments[process.env.NODE_ENV] || '.env',
-      isGlobal: true,
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,20 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('API_KEY') private apiKey: string,
-    private config: ConfigService,
+    //@Inject('API_KEY') private apiKey: string,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
 
   getHello(): string {
-    const apiKey = this.config.get('API_KEY');
+    const apiKey = this.configService.apiKey;
+    const name = this.configService.database.name;
+
     console.log(apiKey);
-    return `Hello World! ${apiKey}`;
+    console.log(name);
+    return `Hello World! ${apiKey} ${name}`;
   }
 }
